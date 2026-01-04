93d6536bbf9a4ef7b354b0da17d5cb35beacc7ef
Bintang Satria Mulya Budi <satriamulya456@gmail.com>
2026-01-04 19:12:45 +0700

    feat: add new feature custom snackbar

--- lib/shared/widgets/custom_snackbar.dart
+++ lib/shared/widgets/custom_snackbar.dart
@@ -1,4 +1,61 @@
+import 'package:chiroku_cafe/shared/style/app_color.dart';
+import 'package:chiroku_cafe/shared/style/google_text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 class CustomSnackbar {
-  
+  //success snackbar
+  void _showSuccessSnackbar(String message) {
+    Get.snackbar(
+      'Success',
+      message,
+      backgroundColor: AppColors.successNormal,
+      colorText: AppColors.white,
+      snackPosition: SnackPosition.TOP,
+      duration: const Duration(seconds: 3),
+      icon: const Icon(Icons.check_circle_outline, color: AppColors.white),
+      borderRadius: 16,
+      margin: const EdgeInsets.all(16),
+      titleText: Text(
+        'Success',
+        style: AppTypography.h6.copyWith(
+          color: AppColors.white,
+        ),
+      ),
+      messageText: Text(
+        message,
+        style: AppTypography.bodyMedium.copyWith(
+          color: AppColors.white,
+        ),
+      ),
+    );
+  }
+
+  // error snackbar
+  void _showErrorSnackbar(String message) {
+    Get.snackbar(
+      'Error',
+      message,
+      backgroundColor: AppColors.alertNormal,
+      colorText: AppColors.white,
+      snackPosition: SnackPosition.TOP,
+      duration: const Duration(seconds: 3),
+      icon: const Icon(Icons.error_outline, color: AppColors.white),
+      borderRadius: 16,
+      margin: const EdgeInsets.all(16),
+      titleText: Text(
+        'Error',
+        style: AppTypography.h6.copyWith(
+          color: AppColors.white,
+        ),
+      ),
+      messageText: Text(
+        message,
+        style: AppTypography.bodyMedium.copyWith(
+          color: AppColors.white,
+        ),
+      ),
+    );
+  }
+
+
 }
